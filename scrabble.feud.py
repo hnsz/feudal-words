8a71b207200bf54866074b7cf99769a59fab129a
hans <hansrudolfw@gmail.com>
2019-10-08 14:53:56 +0200

    Markup scrabble board

--- scrabble.feud.py
+++ scrabble.feud.py
@@ -14,15 +14,36 @@ print(innersq)
 print(margin)
 im = Image.open("wordfeud.jpg")
 
-for N in range(0, nsquares):
-    for n in range(0,nsquares):
-        topleft = offsetx + n * outersq + margin, offsety + N * outersq + margin
+for y in range(0, nsquares):
+    for x in range(0,nsquares):
+        topleft = offsetx + x * outersq + margin, offsety + y * outersq + margin
         bottomright = topleft[0] + innersq, topleft[1] + innersq
         box = topleft + bottomright
 
-        pathout = "square{:02d}x{:02d}.jpeg".format(n +1, N +1)
+        n, N = x + 1, y + 1
+        pathout = "squares/x{:02d}y{:02d}.jpeg".format(n, N)
         imcpy = im.crop(box)
         imcpy.save(pathout)
 
 
+md_out = []
+for y in range(0, nsquares):
+    for x in range(0,nsquares):
+       
+        n, N, S = x + 1, y + 1, x + y * nsquares + 1
 
+        uri = "https://raw.githubusercontent.com/hnsz/feuddle/master/squares/x{:02d}y{:02d}.jpeg".format(n, N)
+        mdtag_f = "![square {:d}]({:s})"
+        mdimg = mdtag_f.format(S, uri)
+        
+        md_out.append(mdimg)
+
+    md_out.append("")
+
+
+
+
+fout = open("README.md", "w")
+print("\n".join(md_out), file=fout )
+
+    
