35ba8597610ce64426167d7229b4365a94d2ddb5
hnsz <hnsz>
2019-10-10 14:38:29 +0200

    cleaner version, added histogram bins

--- scrabble.feud.py
+++ scrabble.feud.py
@@ -1,58 +1,109 @@
 #!/usr/bin/python
+from functools import reduce
+
+import numpy
 from PIL import Image
+import os
 
-offsetx = 4
-offsety = 278 
-nsquares = 15
-lengthboard = 712
-###  705 / 15 = 47
-outersq = lengthboard / nsquares
-innersq = 45
-margin = (outersq - innersq  )
-print(outersq)
-print(innersq)
-print(margin)
-im = Image.open("wordfeud.jpg")
+def incBuckets(l, s):
+    newlist = []
+    start = 0
 
-for y in range(0, nsquares):
-    for x in range(0,nsquares):
-        topleft = offsetx + x * outersq + margin, offsety + y * outersq + margin
-        bottomright = topleft[0] + innersq, topleft[1] + innersq
-        box = topleft + bottomright
+    for subrange in range(s, len(l), s):
+        chunk = l[start:subrange]
+        newlist.append(sum(chunk))
+
+        start = subrange
+    return newlist
+
+
+histogram = []
+basename_f = "x{:02d}y{:02d}{:s}"
+uri_f = "https://raw.githubusercontent.com/hnsz/feuddle/master/squares/{:s}"
+mdtag_f = "![square {:d}]({:s})"
+
+dest_dir_img    = "squares" 
+dest_dir_hist   = "hist"
+sqrt_nsquares = 15
+nsquares = sqrt_nsquares * sqrt_nsquares
+boardposition = 0, 274
+
+
+offsetsx = [  0, 49,  96, 144, 192, 239, 287, 335, 382, 430, 478, 525, 573, 621, 668, 716]
+marginsx = [  4,  3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3   ]
+
+offsetsy = [274, 323, 370, 418, 466, 513, 561, 609, 656, 704, 752, 799, 847, 895, 942, 990]
+marginsy = [  4,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3  ]
+
+
+readme_md = []
+square_data = []
+for y in range(0, sqrt_nsquares):
+    for x in range(0, sqrt_nsquares):
+        left    = offsetsx[x] + marginsx[x]
+        upper   = offsetsy[y] + marginsy[y]
+        right   = offsetsx[x + 1]
+        lower   = offsetsy[y + 1]
 
         n, N = x + 1, y + 1
-        pathout = "squares/x{:02d}y{:02d}.jpeg".format(n, N)
+
+        square_data.append({
+            "box": (left, upper, right, lower),
+            "basename_img": basename_f.format(n, N, ".jpeg"),
+            "basename_hist": basename_f.format(n, N, ".txt")
+            })
+
+
+        m = x + y * sqrt_nsquares + 1
+        uri = uri_f.format(basename_f.format(n, N, ".jpeg"))
+        mdtag_img = mdtag_f.format(m, uri)
+        readme_md.append(mdtag_img)
+
+    readme_md.append("")
+
+
+
+im = Image.open("wordfeud.jpg")
+
+for m in range(0, nsquares):
+        box         = square_data[m]["box"]
+        histpath    = dest_dir_hist + "/" + square_data[m]["basename_hist"]
+        imgpath     = dest_dir_img + "/" + square_data[m]["basename_img"]
+
         imcpy = im.crop(box)
-        imcpy.save(pathout)
+        imcpy.save(imgpath)
 
-        hist = imcpy.histogram() 
-        histpath = "hist/x{:02d}y{:02d}.ddd".format(n, N)
-        histout = open(histpath, "w")
-        print(hist, file=histout)
+        histogram = imcpy.histogram()
+        histo_r = incBuckets(list(histogram[0:256]), 8)
+        histo_g = incBuckets(list(histogram[256:512]),8)
+        histo_b = incBuckets(list(histogram[512:768]), 8)
 
-md_out = []
-for y in range(0, nsquares):
-    for x in range(0,nsquares):
-       
-        n, N, S = x + 1, y + 1, x + y * nsquares + 1
+        hist_fout = open(histpath, "w")
+        print(histo_r, file=hist_fout)
+        print(histo_g, file=hist_fout)
+        print(histo_b, file=hist_fout)
+        hist_fout.close()
 
-        uri = "https://raw.githubusercontent.com/hnsz/feuddle/master/squares/x{:02d}y{:02d}.jpeg".format(n, N)
-        mdtag_f = "![square {:d}]({:s})"
-        mdimg = mdtag_f.format(S, uri)
-        
-        md_out.append(mdimg)
 
-    md_out.append("")
+
+
+
+
 
 
 
 
 fout = open("README.md", "w")
-print("\n".join(md_out), file=fout )
 
-histimg = Image.new("RGBA", 66, 768 )
-histimg.
+print("\n".join(readme_md), file=fout )
 
 
 
-    
+
+#//each value/ bin sum the values
+
+
+#hist_img.
+
+
+
